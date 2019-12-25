f3406d6eb85b8d34c23dd6d8f878297b256fb808
187y1a0575 <g.h.krishnachaitanya@gmail.com>
2019-12-25 23:47:21 +0530

    gql

---
+++ server/resolvers.js
@@ -0,0 +1,10 @@
+const db = require('./db')
+
+const Query = {
+    jobs: () => db.jobs.list()
+};
+const Job = {
+    company: (job) => db.companies.get(job.companyId)
+}
+
+module.exports = {Query, Job};
